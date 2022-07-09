364dcbb5dad30ac147cbe8989145d94c8b7fcbfa
Gakuto1112 <gakuto131112@gmail.com>
2022-07-09 16:03:54 +0900

    ローカルアバターの自動ロードの実装

--- src/main/java/com/github/gakuto1112/figura_prewrite_autoload/FiguraPrewriteAutoload.java
+++ src/main/java/com/github/gakuto1112/figura_prewrite_autoload/FiguraPrewriteAutoload.java
@@ -2,17 +2,28 @@ package com.github.gakuto1112.figura_prewrite_autoload;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.fabricmc.api.ClientModInitializer;
-import com.github.gakuto1112.figura_prewrite_autoload.auto_load.AutoLoad;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.blancworks.figura.avatar.AvatarDataManager;
+import net.blancworks.figura.avatar.LocalAvatarData;
 import com.github.gakuto1112.figura_prewrite_autoload.config.ConfigManager;
 
 public class FiguraPrewriteAutoload implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("figura_prewrite_autoload");
-	public final AutoLoad autoLoad = new AutoLoad();
 	public static final ConfigManager configManager = new ConfigManager();
 
 	@Override
 	public void onInitializeClient() {
-		LOGGER.info("Hello Fabric world!");
+		ClientPlayConnectionEvents.JOIN.register((ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) -> {
+			final String localAvatarPath = FiguraPrewriteAutoload.configManager.getLocalAvatarPath();
+			if(!localAvatarPath.equals("")) {
+				LocalAvatarData localAvatarData = (LocalAvatarData)AvatarDataManager.getDataForPlayer(client.player.getUuid());
+				localAvatarData.isLocalAvatar = true;
+				localAvatarData.loadModelFile(localAvatarPath);
+			}
+		});
 	}
 }
--- src/main/java/com/github/gakuto1112/figura_prewrite_autoload/auto_load/AutoLoad.java
+++
@@ -1,5 +0,0 @@
-package com.github.gakuto1112.figura_prewrite_autoload.auto_load;
-
-public class AutoLoad {
-
-}
